b5a3dbfce3e67a848c833e93c27d59f638b93292
rsundar <rohan.s.1987@gmail.com>
2021-02-07 23:55:28 +0530

    added contact information page in javascript

---
+++ src/contact.js
@@ -0,0 +1,23 @@
+export default function () {
+  const pageContent = document.querySelector('.page-content');
+  pageContent.innerHTML = '';
+  const div = document.createElement('div');
+  div.className = 'columns is-centered';
+  const contactCard = `
+    <div class="column is-half">
+      <div class="content has-text-centered">
+        <p><strong>Find us on all social media and follow us!</strong></p>
+        <br>
+        <p>For party orders or use of the hall please send us an email</p>
+        <p>party@bangaloretiffincenter.com</p>
+        <br>
+        <p>Phone Number</p>
+        <p>+91-9112764532</p>
+        <br>
+        <p>Address</p>
+        <p>Number 10, Shri Kala Building, Lavelle Road, Bengaluru-560012</p>
+      </div>
+    </div>`;
+  div.insertAdjacentHTML('beforeend', contactCard);
+  pageContent.appendChild(div);
+}
